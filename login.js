23f7ddb16f3ca665f7796e63a45499d6c009a014
Your name <Your email>
2023-06-24 01:39:06 +0200

    fixed url issues

--- login.js
+++ login.js
@@ -4,7 +4,7 @@ document.querySelector("#register").addEventListener('click', function(){
     let email = document.querySelector("#registerEmail").value
     let name = document.querySelector('#registerName').value
 
-    fetch('https://weatherapp-backend-cyan.vercel.app/signup',{
+    fetch('https://weatherapp-backend-cyan.vercel.app/users/signup',{
         method:"POST",
         headers:{"Content-Type":"application/json"},
         body: JSON.stringify({ email, password, name })
@@ -22,7 +22,7 @@ document.querySelector("#connection").addEventListener('click', function(){
     let password = document.querySelector("#connectionPassword").value
     let email = document.querySelector("#connectionEmail").value
 
-    fetch('https://weatherapp-backend-cyan.vercel.app/signin',{
+    fetch('https://weatherapp-backend-cyan.vercel.app/users/signin',{
         method:"POST",
         headers:{"Content-Type":"application/json"},
         body: JSON.stringify({ email, password })
